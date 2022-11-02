f0ce94744037d8d5d16a2ddba4e62412c558985d
Xiangz4 <975775303@qq.com>
2022-11-02 22:06:00 +0800

    "config"

---
+++ src/main/java/com/xyf/Main.java
@@ -0,0 +1,13 @@
+package com.xyf;
+
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        HashMap<Object, Object> objectObjectHashMap = new HashMap<>();
+
+        System.out.println(objectObjectHashMap.put(1, 2));
+        System.out.println(objectObjectHashMap.put(1, 2));
+    }
+}
